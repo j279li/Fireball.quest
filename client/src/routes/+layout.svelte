258f837631e4ac5e2f08dcc7f779123b94dc327f
Cheney Cai <cheneycai2004@hotmail.com>
2025-11-04 06:06:37 -0500

    poop

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Button from "$lib/components/ui/button/button.svelte";
 	
 	let { children } = $props();
 </script>
@@ -8,5 +9,11 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<!-- NAV BAR -->
+<nav class="border-b border-gray-200 bg-white p-4 flex gap-4">
+  <a href="/" class="font-semibold">Home</a>
+  <a href="/chat">Chat</a>
+</nav>
 
 {@render children?.()}
