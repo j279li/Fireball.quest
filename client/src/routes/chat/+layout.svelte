66e31a41c0c95416aa7b3994306d0d5e28386762
Adib Raed <adib.raed3@gmail.com>
2025-11-05 20:58:16 -0500

    Create layout for /chat page

---
+++ client/src/routes/chat/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let data;
+  import Sidebar from "./Sidebar.svelte";
+</script>
+
+<!-- Full-height two-column layout -->
+<div class="flex h-[90svh]">
+  <!-- Left: sidebar -->
+  <aside class="w-64 shrink-0 border-r bg-background">
+    <Sidebar />
+  </aside>
+
+  <!-- Right: current /chat page -->
+  <main class="flex-1 min-w-0 flex flex-col">
+    <slot />
+  </main>
+</div>
